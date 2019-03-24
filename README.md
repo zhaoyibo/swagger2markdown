a623f77c474bcd82d7b4fa3256bbbc2d2d940fc8
zhaoyibo <zhaoyibo@moqipobing.com>
2019-03-24 14:22:58 +0800

    update main.go

--- README.md
+++ README.md
@@ -4,19 +4,18 @@ Swagger 文档转 Markdown
 ## Usage
 
 ```
-swagger2markdown version: 1.0
-Usage: swagger2markdown [-h] [--project project] [--url url] [--path] [--file filename]
-Example: swagger2markdown --project wim-manager --path /text/list
+swagger2markdown version: 1.0.0
+Usage:
+  swagger2markdown [OPTIONS]
 
-Options:
-  -file 文件名
-    	保存结果的文件名，例如：wim-manager.md 若文件已存在则会被覆盖
-  -h	帮助
-  -path path 路径
-    	要导出 path 路径，例如：/text/list
-  -project 工程名
-    	要导出文档要的工程名, 例如：wim-manager（若 -project 和 -url 同时存在，则 -url 的优先级更高）
-  -url Swagger API 的 url
-    	要导出文档的Swagger API 的 url, 例如：http://testmanager.wb-intra.com/wim-manager/v2/api-docs
+Application Options:
+  -u, --url=URL            要导出文档的 Swagger API 的 url
+  -p, --project=PROJECT    要导出文档要的工程名, 例如：wim-manager
+                           p.s. -u 的优先级更高
+  -f, --file=FILE          保存结果的文件名，例如：wim-manager.md
+                           若文件已存在则会被覆盖
+  -P, --path=PATH          要导出 path 路径，例如：/text/list
 
+Help Options:
+  -h, --help               Show this help message
 ```
--- main.go
+++ main.go
@@ -1,29 +1,25 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/jessevdk/go-flags"
+	"log"
 	"os"
+	"strings"
+	"windmt.com/swagger2markdown/swagger"
+	"windmt.com/swagger2markdown/tool"
 )
 
-//const url = "http://testmanager.wb-intra.com/wim-manager/v2/api-docs"
-
 var opts struct {
-	Url string `short:"u" long:"url" value-name:"URL" description:"要导出文档的 Swagger API 的 url" long-description:"要导出文档的Swagger API 的 url, 例如：\nhttp://testmanager.wb-intra.com/wim-manager/v2/api-docs"`
-
+	Url     string `short:"u" long:"url" value-name:"URL" description:"要导出文档的 Swagger API 的 url" long-description:"要导出文档的Swagger API 的 url, 例如：\nhttp://testmanager.wb-intra.com/wim-manager/v2/api-docs"`
 	Project string `short:"p" long:"project" value-name:"PROJECT" description:"要导出文档要的工程名, 例如：wim-manager\np.s. -u 的优先级更高"`
-
-	// Example of a value name
-	File string `short:"f" long:"file" description:"保存结果的文件名，例如：wim-manager.md 若文件已存在则会被覆盖" value-name:"FILE"`
-
-	// Example of a required flag
-	Name string `short:"P" long:"path" value-name:"PATH" description:"要导出 path 路径，例如：/text/list"`
+	File    string `short:"f" long:"file" description:"保存结果的文件名，例如：wim-manager.md 若文件已存在则会被覆盖" value-name:"FILE"`
+	Path    string `short:"P" long:"path" value-name:"PATH" description:"要导出 path 路径，例如：/text/list"`
 }
 
 func main() {
-
-	if _, err := flags.Parse(&opts); err != nil {
+	var parser = flags.NewParser(&opts, flags.Default)
+	if _, err := parser.Parse(); err != nil {
 		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
 			os.Exit(0)
 		} else {
@@ -41,45 +37,35 @@ func main() {
 		url = "http://testmanager.wb-intra.com/" + opts.Project + "/v2/api-docs"
 	}
 
-	fmt.Println(url)
-	//
-	//if file != "" {
-	//	if tool.CheckFileIsExist(file) {
-	//		var overwrite string
-	//		fmt.Println("文件已存在，是否确定覆盖: y/N")
-	//		if _, err := fmt.Scanf("%s", &overwrite); err != nil {
-	//			fmt.Println("exit, bye!")
-	//			return
-	//		}
-	//		if !strings.EqualFold(overwrite, "y") && !strings.EqualFold(overwrite, "yes") {
-	//			fmt.Println("exit, bye!")
-	//			return
-	//		}
-	//	}
-	//} else {
-	//	file = tool.ExtractFilename(url, path)
-	//}
-	//
-	//log.Printf("Swagger API 地址：%s\n", url)
-	//log.Printf("保存的文件名：%s\n", file)
-	//
-	//if path == "" {
-	//	if err := swagger.ParseAll(url, file); err != nil {
-	//		log.Fatal("Error:", err)
-	//	}
-	//} else {
-	//	if err := swagger.ParseOne(url, path, file); err != nil {
-	//		log.Fatal("Error:", err)
-	//	}
-	//}
-}
+	file := opts.File
+	path := opts.Path
+	if file != "" {
+		if tool.CheckFileIsExist(file) {
+			var overwrite string
+			fmt.Println("文件已存在，是否确定覆盖: y/N")
+			if _, err := fmt.Scanf("%s", &overwrite); err != nil {
+				fmt.Println("exit, bye!")
+				return
+			}
+			if !strings.EqualFold(overwrite, "y") && !strings.EqualFold(overwrite, "yes") {
+				fmt.Println("exit, bye!")
+				return
+			}
+		}
+	} else {
+		file = tool.ExtractFilename(url, path)
+	}
 
-func usage() {
-	fmt.Fprintf(os.Stderr, `swagger2markdown version: 1.0
-Usage: swagger2markdown [-h] [--project project] [--url url] [--path] [--file filename]
-Example: swagger2markdown --project wim-manager --path /text/list
+	log.Printf("Swagger API 地址：%s\n", url)
+	log.Printf("保存的文件名：%s\n", file)
 
-Options:
-`)
-	flag.PrintDefaults()
+	if path == "" {
+		if err := swagger.ParseAll(url, file); err != nil {
+			log.Fatal("Error:", err)
+		}
+	} else {
+		if err := swagger.ParseOne(url, path, file); err != nil {
+			log.Fatal("Error:", err)
+		}
+	}
 }
